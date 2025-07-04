28c8eb36151dd88bc9c821700e7800925250c774
Shyam Sundar <shyambantu5@gmail.com>
2025-07-04 17:30:16 +0530

    Assignment_2

--- Access2.java
+++
@@ -1,9 +0,0 @@
-import imported.*;
-
-public class Access2 extends Access{
-    public static void main(String[] args) {
-        Access2 a=new Access2();
-        System.out.println("public string outside package is "+a.name2);
-        System.out.println("protected string outside package through object is "+a.name3);
-    }
-}
